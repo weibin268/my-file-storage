970ee60abe328a771c8a82f1c256fa72004ae174
zwb <binweizhuang@hotmail.com>
2023-10-10 20:21:10 +0800

    aa

--- my-file-storage/src/main/java/com/zhuang/filestorage/db/mysql.create.sql
+++ my-file-storage/src/main/java/com/zhuang/filestorage/db/mysql.create.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE IF EXISTS `sys_file_detail`;
 CREATE TABLE `sys_file_detail`
 (
     `id`                varchar(32)  NOT NULL COMMENT '文件id',
@@ -9,19 +9,22 @@ CREATE TABLE `sys_file_detail`
     `base_path`         varchar(256) DEFAULT NULL COMMENT '基础存储路径',
     `path`              varchar(256) DEFAULT NULL COMMENT '存储路径',
     `ext`               varchar(32)  DEFAULT NULL COMMENT '文件扩展名',
-    `content_type`      varchar(128)  DEFAULT NULL COMMENT 'MIME类型',
+    `content_type`      varchar(128) DEFAULT NULL COMMENT 'MIME类型',
     `platform`          varchar(32)  DEFAULT NULL COMMENT '存储平台',
     `th_url`            varchar(512) DEFAULT NULL COMMENT '缩略图访问路径',
     `th_filename`       varchar(256) DEFAULT NULL COMMENT '缩略图名称',
     `th_size`           bigint(20)   DEFAULT NULL COMMENT '缩略图大小，单位字节',
-    `th_content_type`   varchar(128)  DEFAULT NULL COMMENT '缩略图MIME类型',
+    `th_content_type`   varchar(128) DEFAULT NULL COMMENT '缩略图MIME类型',
     `object_id`         varchar(32)  DEFAULT NULL COMMENT '文件所属对象id',
     `object_type`       varchar(32)  DEFAULT NULL COMMENT '文件所属对象类型，例如用户头像，评价图片',
-    `attr`              text    COMMENT '附加属性',
+    `attr`              text COMMENT '附加属性',
     `file_acl`          varchar(32)  DEFAULT NULL COMMENT '文件ACL',
     `th_file_acl`       varchar(32)  DEFAULT NULL COMMENT '缩略图文件ACL',
-    `create_time`       datetime    DEFAULT NULL COMMENT '创建时间',
-    `modify_time`       datetime    DEFAULT NULL COMMENT '修改时间',
-    `status`            tinyint     DEFAULT NULL COMMENT '状态：0=未提交；1=已提交；',
-    PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 ROW_FORMAT = DYNAMIC COMMENT ='文件记录表';
+    `create_time`       datetime     DEFAULT NULL COMMENT '创建时间',
+    `modify_time`       datetime     DEFAULT NULL COMMENT '修改时间',
+    `status`            tinyint(4)   DEFAULT NULL COMMENT '状态：0=未提交；1=已提交；',
+    PRIMARY KEY (`id`) USING BTREE,
+    KEY `idx_object_type_object_id` (`object_type`, `object_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC COMMENT ='文件上传记录表';
