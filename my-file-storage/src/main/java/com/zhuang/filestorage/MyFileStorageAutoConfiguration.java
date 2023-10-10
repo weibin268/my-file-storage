3efc22af30855b0dc8a4fe91c6244d4fa8e448f9
庄伟斌 <zhuangweibin@qq.com>
2023-10-10 11:53:34 +0800

    aa

--- my-file-storage/src/main/java/com/zhuang/filestorage/MyFileStorageAutoConfiguration.java
+++ my-file-storage/src/main/java/com/zhuang/filestorage/MyFileStorageAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.zhuang.filestorage;
 
+import cn.xuyanwu.spring.file.storage.spring.EnableFileStorage;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
+@EnableFileStorage
 public class MyFileStorageAutoConfiguration {
 
 }
--- my-file-storage/src/main/java/com/zhuang/filestorage/controller/FileStorageController.java
+++ my-file-storage/src/main/java/com/zhuang/filestorage/controller/FileStorageController.java
@@ -1,14 +1,26 @@
 package com.zhuang.filestorage.controller;
 
+import cn.xuyanwu.spring.file.storage.FileInfo;
 import cn.xuyanwu.spring.file.storage.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("fileStorage")
 public class FileStorageController {
 
     @Autowired
     private FileStorageService fileStorageService;
 
+    /**
+     * 上传文件
+     */
+    @PostMapping("/upload")
+    public FileInfo upload(MultipartFile file) {
+        return fileStorageService.of(file).upload();
+    }
 
 }
